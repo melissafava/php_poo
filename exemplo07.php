8e10ed7087c01ae640920a971cdf71e5dfafa8e7
Melissa <melissa.fava@aluno.senai.br>
2026-03-13 09:46:51 -0300

    6 ao 8

---
+++ exemplo07.php
@@ -0,0 +1,18 @@
+<?php
+
+class Contra
+{
+    public $numero;
+    public $saldo;
+
+    function __construct($numero, $saldo)
+    {
+        $this->numero = $numero;
+        $this->saldo = $saldo;
+    }
+
+    function creditar($valor)
+    {
+        $this->saldo = $this->saldo + $valor;
+    }
+}
