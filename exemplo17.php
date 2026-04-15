d962214e57377d53afa2cd6b06d03c69c8080ae4
Melissa <melissa.fava@aluno.senai.br>
2026-04-15 11:21:50 -0300

    commit

--- exemplo17.php
+++ exemplo17.php
@@ -11,9 +11,28 @@ class Visibilidade {
         $this->varProtected = $varProtected;
         $this->varPrivate = $varPrivate;
     }
+
+    public function publicFunc(){
+        echo "Método Público<br>";
+    }
+
+    protected function protectedFunc(){
+        echo "Método Protegido<br>";
+    }
+
+    private function privateFunc(){
+        echo "Método Privado<br>";
+    }
 }
 
 $teste = new Visibilidade(1,2,3);
+
 echo "Atributo Public = $teste->varPublic";
 echo "Atributo Protected = $teste->varProtected";
 echo "Atributo Private = $teste->varPrivate";
+
+echo "<br>";
+
+$teste->publicFunc();
+$teste->proctectedFunc();
+$teste->privateFunc();
