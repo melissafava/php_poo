3799c22809bba847d38da91f3cfcf65793254573
Melissa <melissa.fava@aluno.senai.br>
2026-04-17 08:36:18 -0300

    ok

--- carro.php
+++ carro.php
@@ -1,12 +1,29 @@
 <?php
 class Carro {
     // 🚩 RISCO: Atributo público permite valores ilegais
-    public $modelo;
+    private $modelo;
     private $velocidade;
 
     public function __construct($modelo, $velocidade) {
         $this->modelo = $modelo;
         $this->velocidade = $velocidade;
+    }
+
+    get velocidade
+     public function getVelocidade()
+    {
+        return $this->velocidade;
+    }
+
+    get modelo
+     public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    public function $novaVelocidade()
+    {
+        if(novaVelocidade > 0 && novaVelocidade < 201)
     }
 }
 
